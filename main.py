4fe5345abb303f3affb94d506d557f271cff3227
Elzar2008 <elzar.kubanychbekovich@gmail.com>
2023-04-30 23:51:17 +0600

    HW7

--- main.py
+++ main.py
@@ -1,154 +1,113 @@
 import sqlite3
-from sqlite3 import Error
 
 
-def create_connection(db_name):
-    conn = None
-    try:
-        conn = sqlite3.connect(db_name)
-    except Error:
-        print(Error)
-    return conn
+conn = sqlite3.connect('database.db')
+cursor = conn.cursor()
 
 
-def create_table(conn, sql):
-    try:
-        cursor = conn.cursor()
-        cursor.execute(sql)
-    except Error:
-        print(Error)
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS countries (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title TEXT NOT NULL
+    )
+''')
 
 
-def create_product(conn, product):
-    try:
-        sql = '''INSERT INTO products 
-        (product_title, price, quantity)
-        VALUES (?, ?, ?)'''
-        cursor = conn.cursor()
-        cursor.execute(sql, product)
-        conn.commit()
-    except Error:
-        print(Error)
+cursor.execute("INSERT INTO countries (title) VALUES ('Кыргызстан')")
+cursor.execute("INSERT INTO countries (title) VALUES ('Германия')")
+cursor.execute("INSERT INTO countries (title) VALUES ('Китай')")
 
 
-def create_products(conn):
-    try:
-        create_product(conn, ("Алькони", 78.2, 7))
-        create_product(conn, ("Сосиски", 250.4, 26))
-        create_product(conn, ("Колбаса", 300.5, 16))
-        create_product(conn, ("Хлеб", 30.5, 50))
-        create_product(conn, ("Сыр", 125.5, 25))
-        create_product(conn, ("молоко", 15.5, 30))
-        create_product(conn, ("Кефир", 50.5, 15))
-        create_product(conn, ("Жвачка", 5.5, 10))
-        create_product(conn, ("Чипсы", 220.5, 5))
-        create_product(conn, ("Фанта", 85.5, 12))
-        create_product(conn, ("Сырок", 85.5, 12))
-        create_product(conn, ("Pepsi", 110.5, 12))
-        create_product(conn, ("Сникерс", 30.6, 20))
-        create_product(conn, ("Альбени", 27.3, 20))
-        create_product(conn, ("Йогурт", 25.5, 10))
-    except Error:
-        print(Error)
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS cities (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title TEXT NOT NULL,
+        area REAL DEFAULT 0,
+        country_id INTEGER,
+        FOREIGN KEY (country_id) REFERENCES countries (id)
+    )
+''')
+
+cursor.execute("INSERT INTO cities (title, country_id) VALUES ('Бишкек', 1)")
+cursor.execute("INSERT INTO cities (title, country_id) VALUES ('Ош', 2)")
+cursor.execute("INSERT INTO cities (title, country_id) VALUES ('Берлин', 3)")
+cursor.execute("INSERT INTO cities (title, country_id) VALUES ('Пекин', 4)")
 
 
-def update_product_quantity(conn, product):
-    try:
-        sql = '''UPDATE products SET quantity = ? WHERE id = ?'''
-        cursor = conn.cursor()
-        cursor.execute(sql, product)
-        conn.commit()
-    except Error:
-        print(Error)
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS employees (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        first_name TEXT NOT NULL,
+        last_name TEXT NOT NULL,
+        city_id INTEGER,
+        FOREIGN KEY (city_id) REFERENCES cities (id)
+    )
+''')
+
+cursor.execute("INSERT INTO employees (first_name, last_name, city_id) VALUES ('Иван', 'Грозный', 1)")
+cursor.execute("INSERT INTO employees (first_name, last_name, city_id) VALUES ('Петр', 'Первый', 2)")
+cursor.execute("INSERT INTO employees (first_name, last_name, city_id) VALUES ('Жылдыз', 'Адбразакова', 4)")
+cursor.execute("INSERT INTO employees (first_name, last_name, city_id) VALUES ('Санька', 'Пушкин', 3)")
 
 
-def search_by_word(conn, word):
-    try:
-        sql = '''SELECT * FROM products WHERE product_title LIKE ?'''
-        cursor = conn.cursor()
-        cursor.execute(sql, ('%' + word + '%',))
-
-        rows = cursor.fetchall()
-        for row in rows:
-            print(row)
-    except Error as e:
-        print(e)
+conn.commit()
+conn.close()
 
 
-def update_product_price(conn, product):
-    try:
-        sql = '''UPDATE products SET price = ? WHERE id = ?'''
-        cursor = conn.cursor()
-        cursor.execute(sql, product)
-        conn.commit()
-    except Error:
-        print(Error)
+print("Вы можете отобразить список сотрудников по выбранному id города из перечня городов ниже, для выхода из программы введите 0:")
 
 
-def delete_products(conn, id):
-    try:
-        sql = '''DELETE FROM products WHERE id = ?'''
-        cursor = conn.cursor()
-        cursor.execute(sql, (id,))
-        conn.commit()
-    except Error:
-        print(Error)
+conn = sqlite3.connect('database.db')
+cursor = conn.cursor()
+cursor.execute("SELECT title FROM cities")
+cities = cursor.fetchall()
+conn.close()
+
+print("Список городов:")
+for city in set(cities):
+    print(city[0])
 
 
-def select_all_products(conn):
-    try:
-        sql = '''SELECT * FROM products'''
-        cursor = conn.cursor()
-        cursor.execute(sql)
-
-        product_list = cursor.fetchall()
-        for product in product_list:
-            print(product)
-    except Error:
-        print(Error)
+while True:
+    city_id = input("Введите id города (для выхода введите 0): ")
+    if city_id == '0':
+        break
 
 
-def select_all_products_by_quantity_and_price(conn):
-    try:
-        sql = '''SELECT * FROM products WHERE quantity > 5 and price < 100.00'''
-        cursor = conn.cursor()
-        cursor.execute(sql)
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+    cursor.execute('''
+        SELECT employees.first_name, employees.last_name, countries.title, cities.title
+        FROM employees
+        INNER JOIN cities ON employees.city_id = cities.id
+        INNER JOIN countries ON cities.country_id = countries.id
+        WHERE cities.id = ?
+    ''', (city_id,))
 
-        rows = cursor.fetchall()
-        print(rows)
-
-    except Error:
-        print(Error)
-
-def search_by_word(conn, word):
-    try:
-        sql = '''SELECT * FROM products WHERE product_title LIKE ?'''
-        cursor = conn.cursor()
-        cursor.execute(sql, ('%' + word + '%',))
-
-        rows = cursor.fetchall()
-        for row in rows:
-            print(row)
-    except Error:
-        print(Error)
+    employees = cursor.fetchall()
+    conn.close()
 
 
-connection = create_connection("hw.db")
+    if len(employees) == 0:
+        print("Нет сотрудников в данном городе.")
+    else:
+        print("Сотрудники в выбранном городе:")
+        for employee in set(employees):
+            print("Имя:", employee[0])
+            print("Фамилия:", employee[1])
+            print("Страна:", employee[2])
+            print("Город:", employee[3])
+            print()
 
-create_products_table = '''
-CREATE TABLE products (
-id INTEGER PRIMARY KEY AUTOINCREMENT,
-product_title VARCHAR (200) NOT NULL,
-price DOUBLE (10, 2) NOT NULL DEFAULT 0.0,
-quantity INTEGER NOT NULL DEFAULT 0
-)
-'''
 
-if connection is not None:
-    print("Connected Success!")
-    select_all_products(connection)
-    delete_products(connection, 14)
-    create_products(connection)
-    update_product_quantity(connection, (8, 7))
-    update_product_price(connection, (45, 1))
-    select_all_products(connection)
+
+
+
+
+
+
+
+
+
+
+
